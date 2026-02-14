97f98647411b0d7755e793d6150ab8eec25b4cdd
LAB13_USER ON WS20 <L13Y17W20@LabHCI.net>
2026-02-14 17:30:40 +0800

    frontend

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './pages/login'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
  
   return (
-    <>
-      
-    </>
+    <Router>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </Router>
   )
 }
 
